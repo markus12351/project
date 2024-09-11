b575e17cca1dc9fab69391f9da583562b518a232
markus12351 <btanja525@gmail.com>
2024-09-12 00:39:57 +0200

    Update RestaurantServiceInterface.java

--- src/main/java/com/example/backend4rate/services/RestaurantServiceInterface.java
+++ src/main/java/com/example/backend4rate/services/RestaurantServiceInterface.java
@@ -1,12 +1,17 @@
 package com.example.backend4rate.services;
 
-import com.example.backend4rate.models.entities.RestaurantEntity;
+import com.example.backend4rate.models.dto.Restaurant;
+import com.example.backend4rate.models.entities.CategoryEntity;
+
 import java.util.List;
 
 public interface RestaurantServiceInterface {
-    List<RestaurantEntity> getAllRestaurants();
-    RestaurantEntity addRestaurant(RestaurantEntity restaurant);
-    RestaurantEntity getRestaurantById(Integer id);
-    RestaurantEntity updateRestaurant(Integer id, RestaurantEntity restaurant);
-    void deleteRestaurant(Integer id);
+    List<Restaurant> searchRestaurant(String name);
+    Restaurant getRestaurant(int idRestaurant);
+    List<Restaurant> getAllRestaurants();
+    List<Restaurant> getFavoriteRestaurants();
+    boolean addFavoriteRestaurant(int idRestaurant);
+    boolean updateRestaurantInformation(Restaurant restaurant);
+    boolean removeFavoriteRestaurant(int idRestaurant);
+    List<Restaurant> getAllRestaurants(List<CategoryEntity> categories);
 }
