e42cb1663361300027b10e1f49959caa563eee4d
markus12351 <btanja525@gmail.com>
2024-09-11 23:13:26 +0200

    Update RestaurantServiceInterface.java

--- src/main/java/com/example/backend4rate/services/RestaurantServiceInterface.java
+++ src/main/java/com/example/backend4rate/services/RestaurantServiceInterface.java
@@ -1,1 +1,11 @@
+package com.example.backend4rate.services;
 
+import com.example.backend4rate.models.entities.RestaurantEntity;
+import java.util.List;
+
+public interface RestaurantServiceInterface {
+    List<RestaurantEntity> getAllRestaurants();
+    RestaurantEntity addRestaurant(RestaurantEntity restaurant);
+    RestaurantEntity getRestaurantById(Integer id);
+    void deleteRestaurant(Integer id);
+}
