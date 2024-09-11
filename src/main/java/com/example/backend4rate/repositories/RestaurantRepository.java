6bf2a5809e1da0e8d123417fe6f45f7a4dd0921d
markus12351 <btanja525@gmail.com>
2024-09-11 23:52:09 +0200

    Update RestaurantRepository.java

--- src/main/java/com/example/backend4rate/repositories/RestaurantRepository.java
+++ src/main/java/com/example/backend4rate/repositories/RestaurantRepository.java
@@ -1,1 +1,9 @@
+package com.example.backend4rate.repositories;
 
+import com.example.backend4rate.models.entities.RestaurantEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<RestaurantEntity, Integer> {
+}
