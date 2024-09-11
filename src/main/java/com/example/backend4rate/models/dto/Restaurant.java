6becdd56cb6c807e590f97fe9e8e8d17ae581d58
markus12351 <btanja525@gmail.com>
2024-09-12 00:37:07 +0200

    Update Restaurant.java

--- src/main/java/com/example/backend4rate/models/dto/Restaurant.java
+++ src/main/java/com/example/backend4rate/models/dto/Restaurant.java
@@ -1,1 +1,12 @@
+package com.example.backend4rate.models.dto;
 
+import lombok.Data;
+
+@Data
+public class Restaurant {
+    private Integer id;
+    private String name;
+    private String description;
+    private String workTime;
+    // Dodaj ostala polja prema potrebi
+}
